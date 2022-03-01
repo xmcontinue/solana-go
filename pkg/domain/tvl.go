c95c380f34babc9d133201ec660b168438ae5551
yaobo <yaobo@cplus.com>
2022-03-01 20:59:56 +0800

    修改索引名

--- pkg/domain/tvl.go
+++ pkg/domain/tvl.go
@@ -22,15 +22,15 @@ type SwapCount struct {
 	ID                    int64           `json:"-" gorm:"primaryKey;auto_increment"` // 自增主键，自增主键不能有任何业务含义。
 	CreatedAt             *time.Time      `json:"-" gorm:"not null;type:timestamp(6);index"`
 	UpdatedAt             *time.Time      `json:"-" gorm:"not null;type:timestamp(6);index"`
-	LastSwapTransactionID int64           `json:"last_swap_transaction_id" gorm:"not null;default:0"`                                               // 最后同步的transaction id
-	SwapAddress           string          `json:"swap_address" gorm:"not null;type:varchar(64);uniqueIndex:swap_tvl_count_swap_address_unique_key"` // swap地址
-	TokenAAddress         string          `json:"token_a_address" gorm:"not null;type:varchar(64);  index"`                                         // swap token a 地址
-	TokenBAddress         string          `json:"token_b_address" gorm:"not null;type:varchar(64);  index"`                                         // swap token b 地址
-	TokenAVolume          decimal.Decimal `json:"token_a_volume" gorm:"type:decimal(36,18);default:0"`                                              // swap token a 总交易额
-	TokenBVolume          decimal.Decimal `json:"token_b_volume" gorm:"type:decimal(36,18);default:0"`                                              // swap token b 总交易额
-	TokenABalance         decimal.Decimal `json:"token_a_balance" gorm:"type:decimal(36,18);default:0"`                                             // swap token a 余额
-	TokenBBalance         decimal.Decimal `json:"token_b_balance" gorm:"type:decimal(36,18);default:0"`                                             // swap token b 余额
-	TxNum                 int64           `json:"tx_num"`                                                                                           // 交易笔数
+	LastSwapTransactionID int64           `json:"last_swap_transaction_id" gorm:"not null;default:0"`                                           // 最后同步的transaction id
+	SwapAddress           string          `json:"swap_address" gorm:"not null;type:varchar(64);uniqueIndex:swap_count_swap_address_unique_key"` // swap地址
+	TokenAAddress         string          `json:"token_a_address" gorm:"not null;type:varchar(64);  index"`                                     // swap token a 地址
+	TokenBAddress         string          `json:"token_b_address" gorm:"not null;type:varchar(64);  index"`                                     // swap token b 地址
+	TokenAVolume          decimal.Decimal `json:"token_a_volume" gorm:"type:decimal(36,18);default:0"`                                          // swap token a 总交易额
+	TokenBVolume          decimal.Decimal `json:"token_b_volume" gorm:"type:decimal(36,18);default:0"`                                          // swap token b 总交易额
+	TokenABalance         decimal.Decimal `json:"token_a_balance" gorm:"type:decimal(36,18);default:0"`                                         // swap token a 余额
+	TokenBBalance         decimal.Decimal `json:"token_b_balance" gorm:"type:decimal(36,18);default:0"`                                         // swap token b 余额
+	TxNum                 int64           `json:"tx_num"`                                                                                       // 交易笔数
 }
 
 type SwapCountKLine struct {
