899e65ffb020316d6fe0e4623398414191bbceff
maxinyu <408567964@qq.com>
2022-03-02 21:14:05 +0800

    加入流动性解析

--- chain/sol/transaction_parse.go
+++ chain/sol/transaction_parse.go
@@ -9,11 +9,12 @@ import (
 )
 
 type Tx struct {
-	Data        rpc.GetTransactionResult
-	SwapRecords []*SwapRecord
+	Data             rpc.GetTransactionResult
+	SwapRecords      []*SwapRecord
+	LiquidityRecords []*LiquidityRecord
 }
 
-// SwapRecord 解析后的有用数据
+// SwapRecord 解析后的swap数据
 type SwapRecord struct {
 	UserOwnerAddress  string
 	UserTokenAAddress string
@@ -43,31 +44,44 @@ type SwapIndex struct {
 	TokenBIndex      int64
 }
 
+// LiquidityRecord 解析后的liquidity数据
+type LiquidityRecord struct {
+	UserOwnerAddress  string
+	UserTokenAAddress string
+	UserTokenBAddress string
+	ProgramAddress    string
+	Direction         int8 // 0为取出,1为质押
+	UserCount         *SwapCount
+	TokenCount        *SwapCount
+	SwapConfig        *SwapConfig
+}
+
 var (
 	cremaSwapProgramAddress = "6MLxLqiXaaSUpkgMnWDTuejNZEz3kE7k2woyHGVFw319"
 )
 
 func NewTx(txData *domain.TxData) *Tx {
 	return &Tx{
-		Data:        rpc.GetTransactionResult(*txData),
-		SwapRecords: make([]*SwapRecord, 0),
+		Data:             rpc.GetTransactionResult(*txData),
+		SwapRecords:      make([]*SwapRecord, 0),
+		LiquidityRecords: make([]*LiquidityRecord, 0),
 	}
 }
 
 // ParseTxToSwap 解析TX到swap
 func (t *Tx) ParseTxToSwap() error {
 	// parser instructions
-
 	accountKeys := t.Data.Transaction.GetParsedTransaction().Message.AccountKeys
 
 	for _, instruction := range t.Data.Transaction.GetParsedTransaction().Message.Instructions {
+		t.parseInstructionToSwapRecord(instruction)
 		// 仅已知的swap address 才可以解析
 		programAddress := accountKeys[instruction.ProgramIDIndex].String()
 		if programAddress != cremaSwapProgramAddress {
 			continue
 		}
 
-		// swap 函数在合约里面的下表是1
+		// swap 函数在合约里面的是01
 		if instruction.Data[0] != 1 {
 			continue
 		}
@@ -108,12 +122,11 @@ func (t *Tx) ParseTxToSwap() error {
 		for _, compiledInstruction := range innerInstruction.Instructions {
 
 			programAddress := accountKeys[compiledInstruction.ProgramIDIndex].String()
-
 			if programAddress != cremaSwapProgramAddress {
 				continue
 			}
 
-			// swap 函数在合约里面的下表是1
+			// swap 函数在合约里面的是01
 			if compiledInstruction.Data[0] != 1 {
 				continue
 			}
@@ -149,7 +162,7 @@ func (t *Tx) ParseTxToSwap() error {
 		}
 	}
 
-	err := t.parseSwapRecord()
+	err := t.calculateSwap()
 	if err != nil {
 		return errors.Wrap(err)
 	}
@@ -157,16 +170,22 @@ func (t *Tx) ParseTxToSwap() error {
 	return nil
 }
 
-// parseSwapRecord 解析出Swap有用的信息
-func (t *Tx) parseSwapRecord() error {
+func (t *Tx) parseInstructionToSwapRecord(data interface{}) *SwapRecord {
+	// solana.CompiledInstruction{}
+	// rpc.ParsedInstruction{}
+	return &SwapRecord{}
+}
+
+// calculateSwaps ...
+func (t *Tx) calculateSwap() error {
 	if len(t.SwapRecords) == 0 {
 		return errors.RecordNotFound
 	}
 
 	for _, v := range t.SwapRecords {
-		t.swapCalculate(v, v.UserCount)
+		t.calculate(v.UserCount)
 
-		t.swapCalculate(v, v.TokenCount)
+		t.calculate(v.TokenCount)
 
 		if v.TokenCount.TokenAVolume.IsZero() {
 			continue
@@ -177,46 +196,39 @@ func (t *Tx) parseSwapRecord() error {
 	return nil
 }
 
-func (t *Tx) swapCalculate(swapRecord *SwapRecord, swapCount *SwapCount) {
+func (t *Tx) calculate(swapCount *SwapCount) {
 	var (
-		TokenAPreVolume  decimal.Decimal
-		TokenBPreVolume  decimal.Decimal
-		TokenAPostVolume decimal.Decimal
-		TokenBPostVolume decimal.Decimal
+		TokenAPreVolume, TokenBPreVolume, TokenAPostVolume, TokenBPostVolume decimal.Decimal
 	)
 
 	for _, preVal := range t.Data.Meta.PreTokenBalances {
 		if swapCount.TokenAIndex == int64(preVal.AccountIndex) {
-			TokenAPreVolume, _ = decimal.NewFromString(preVal.UiTokenAmount.Amount)
-			TokenAPreVolume = TokenAPreVolume.Abs()
+			TokenAPreVolume = decimal.NewFromFloat(*preVal.UiTokenAmount.UiAmount).Abs()
 			continue
 		}
 
 		if swapCount.TokenBIndex == int64(preVal.AccountIndex) {
-			TokenBPreVolume, _ = decimal.NewFromString(preVal.UiTokenAmount.Amount)
-			TokenBPreVolume = TokenBPreVolume.Abs()
+			TokenBPreVolume = decimal.NewFromFloat(*preVal.UiTokenAmount.UiAmount).Abs()
 			continue
 		}
 	}
 
 	for _, postVal := range t.Data.Meta.PostTokenBalances {
 		if swapCount.TokenAIndex == int64(postVal.AccountIndex) {
-			TokenAPostVolume, _ = decimal.NewFromString(postVal.UiTokenAmount.Amount)
-			TokenAPostVolume = TokenAPostVolume.Abs()
+			TokenAPostVolume = decimal.NewFromFloat(*postVal.UiTokenAmount.UiAmount).Abs()
 			continue
 		}
 
 		if swapCount.TokenBIndex == int64(postVal.AccountIndex) {
-			TokenBPostVolume, _ = decimal.NewFromString(postVal.UiTokenAmount.Amount)
-			TokenBPostVolume = TokenBPostVolume.Abs()
+			TokenBPostVolume = decimal.NewFromFloat(*postVal.UiTokenAmount.UiAmount).Abs()
 			continue
 		}
 	}
 
-	swapCount.TokenAVolume = precisionConversion(TokenAPostVolume.Sub(TokenAPreVolume), int(swapRecord.SwapConfig.TokenA.Decimal))
-	swapCount.TokenBVolume = precisionConversion(TokenBPostVolume.Sub(TokenBPreVolume), int(swapRecord.SwapConfig.TokenB.Decimal))
-	swapCount.TokenABalance = precisionConversion(TokenAPostVolume, int(swapRecord.SwapConfig.TokenA.Decimal))
-	swapCount.TokenBBalance = precisionConversion(TokenBPostVolume, int(swapRecord.SwapConfig.TokenB.Decimal))
+	swapCount.TokenAVolume = TokenAPostVolume.Sub(TokenAPreVolume)
+	swapCount.TokenBVolume = TokenBPostVolume.Sub(TokenBPreVolume)
+	swapCount.TokenABalance = TokenAPostVolume
+	swapCount.TokenBBalance = TokenBPostVolume
 }
 
 func (sr *SwapRecord) GetVol() decimal.Decimal {
@@ -224,4 +236,130 @@ func (sr *SwapRecord) GetVol() decimal.Decimal {
 		return sr.TokenCount.TokenAVolume
 	}
 	return sr.TokenCount.TokenBVolume
+}
+
+// ParseTxToLiquidity 解析TX到流动性
+func (t *Tx) ParseTxToLiquidity() error {
+	// parser instructions
+	accountKeys := t.Data.Transaction.GetParsedTransaction().Message.AccountKeys
+
+	for _, instruction := range t.Data.Transaction.GetParsedTransaction().Message.Instructions {
+		// 仅已知的swap address 才可以解析
+		programAddress := accountKeys[instruction.ProgramIDIndex].String()
+		if programAddress != cremaSwapProgramAddress {
+			continue
+		}
+
+		// Liquidity 函数在合约里面的是02,03
+		if instruction.Data[0] != 2 && instruction.Data[0] != 3 {
+			continue
+		}
+
+		var (
+			cremaLiquidityIndex *SwapIndex
+			direction           int8
+		)
+
+		if instruction.Data[0] == 02 {
+			cremaLiquidityIndex = &SwapIndex{0, 3, 4, 5, 6}
+			direction = 1
+		} else {
+			cremaLiquidityIndex = &SwapIndex{0, 7, 8, 5, 6}
+			direction = 0
+		}
+
+		swapConfig, ok := swapConfigMap[accountKeys[instruction.Accounts[cremaLiquidityIndex.SwapAddressIndex]].String()]
+		if !ok {
+			return errors.RecordNotFound
+		}
+
+		t.LiquidityRecords = append(t.LiquidityRecords, &LiquidityRecord{
+			UserOwnerAddress:  accountKeys[0].String(),
+			SwapConfig:        swapConfig,
+			UserTokenAAddress: accountKeys[instruction.Accounts[cremaLiquidityIndex.UserTokenAIndex]].String(),
+			UserTokenBAddress: accountKeys[instruction.Accounts[cremaLiquidityIndex.UserTokenBIndex]].String(),
+			ProgramAddress:    programAddress,
+			Direction:         direction,
+			UserCount: &SwapCount{
+				TokenAIndex: instruction.Accounts[cremaLiquidityIndex.UserTokenAIndex],
+				TokenBIndex: instruction.Accounts[cremaLiquidityIndex.UserTokenBIndex],
+			},
+			TokenCount: &SwapCount{
+				TokenAIndex: instruction.Accounts[cremaLiquidityIndex.TokenAIndex],
+				TokenBIndex: instruction.Accounts[cremaLiquidityIndex.TokenBIndex],
+			},
+		})
+	}
+
+	for _, innerInstruction := range t.Data.Meta.InnerInstructions {
+		// 仅已知的swap address 才可以解析
+		for _, compiledInstruction := range innerInstruction.Instructions {
+			programAddress := accountKeys[compiledInstruction.ProgramIDIndex].String()
+			if programAddress != cremaSwapProgramAddress {
+				continue
+			}
+
+			// Liquidity 函数在合约里面的是02,03
+			if compiledInstruction.Data[0] != 2 && compiledInstruction.Data[0] != 3 {
+				continue
+			}
+
+			var (
+				cremaLiquidityIndex *SwapIndex
+				direction           int8
+			)
+
+			if compiledInstruction.Data[0] == 02 {
+				cremaLiquidityIndex = &SwapIndex{0, 3, 4, 5, 6}
+				direction = 1
+			} else {
+				cremaLiquidityIndex = &SwapIndex{0, 7, 8, 5, 6}
+				direction = 0
+			}
+
+			swapConfig, ok := swapConfigMap[accountKeys[compiledInstruction.Accounts[cremaLiquidityIndex.SwapAddressIndex]].String()]
+			if !ok {
+				return errors.RecordNotFound
+			}
+
+			t.LiquidityRecords = append(t.LiquidityRecords, &LiquidityRecord{
+				UserOwnerAddress:  accountKeys[0].String(),
+				SwapConfig:        swapConfig,
+				UserTokenAAddress: accountKeys[compiledInstruction.Accounts[cremaLiquidityIndex.UserTokenAIndex]].String(),
+				UserTokenBAddress: accountKeys[compiledInstruction.Accounts[cremaLiquidityIndex.UserTokenBIndex]].String(),
+				ProgramAddress:    programAddress,
+				Direction:         direction,
+				UserCount: &SwapCount{
+					TokenAIndex: int64(compiledInstruction.Accounts[cremaLiquidityIndex.UserTokenAIndex]),
+					TokenBIndex: int64(compiledInstruction.Accounts[cremaLiquidityIndex.UserTokenBIndex]),
+				},
+				TokenCount: &SwapCount{
+					TokenAIndex: int64(compiledInstruction.Accounts[cremaLiquidityIndex.TokenAIndex]),
+					TokenBIndex: int64(compiledInstruction.Accounts[cremaLiquidityIndex.TokenBIndex]),
+				},
+			})
+		}
+	}
+
+	err := t.calculateLiquidity()
+	if err != nil {
+		return errors.Wrap(err)
+	}
+
+	return nil
+}
+
+// calculateLiquidity ...
+func (t *Tx) calculateLiquidity() error {
+	if len(t.LiquidityRecords) == 0 {
+		return errors.RecordNotFound
+	}
+
+	for _, v := range t.LiquidityRecords {
+		t.calculate(v.UserCount)
+
+		t.calculate(v.TokenCount)
+	}
+
+	return nil
 }
