4d09de006ea005e18e00636b158d30d821b32002
maxinyu <408567964@qq.com>
2022-03-10 11:02:06 +0800

    redis key加前缀

--- pkg/domain/redis.go
+++ pkg/domain/redis.go
@@ -11,31 +11,33 @@ type RedisKey struct {
 	Size    int64
 }
 
+const publicPrefix = "crema:"
+
 // SwapVolCountLast24HKey 最近24小时的总的交易额
 func SwapVolCountLast24HKey(accountAddress string) RedisKey {
-	key := fmt.Sprintf("swap:vol:count:last24h:%s", accountAddress)
+	key := fmt.Sprintf("%sswap:vol:count:last24h:%s", publicPrefix, accountAddress)
 	return RedisKey{key, time.Hour * 1, 0}
 }
 
 // SwapCountKey swap account 的锁仓量
 func SwapCountKey(swapAddress string) RedisKey {
-	key := fmt.Sprintf("swap:tvl:%s", swapAddress)
+	key := fmt.Sprintf("%sswap:tvl:%s", publicPrefix, swapAddress)
 	return RedisKey{key, 0, 0}
 }
 
 // SwapTotalCountKey swap 总统计的redis key
 func SwapTotalCountKey() RedisKey {
-	return RedisKey{"swap:count:total", time.Hour, 0}
+	return RedisKey{fmt.Sprintf("%sswap:count:total", publicPrefix), time.Hour, 0}
 }
 
 // AccountSwapVolCountKey 总交易额
 // 使用两个accountAddress，当获取swap account时，第二个为空，
 // 当获取user account 时，第一个表示useraccount ，第二个表示对应的swap account 地址，因为一个user swap 可能参与多个swap 交易
 func AccountSwapVolCountKey(accountAddress1, accountAddress2 string) RedisKey {
-	key := fmt.Sprintf("swap:vol:%s:%s", accountAddress1, accountAddress2)
+	key := fmt.Sprintf("%sswap:vol:%s:%s", publicPrefix, accountAddress1, accountAddress2)
 	return RedisKey{key, 0, 0}
 }
 
 func KLineKey(dateType DateType, swapAccount string) string {
-	return fmt.Sprintf("kline:swap:count:%s:%s", dateType, swapAccount)
+	return fmt.Sprintf("%sswap:kline:count:%s:%s", publicPrefix, dateType, swapAccount)
 }
