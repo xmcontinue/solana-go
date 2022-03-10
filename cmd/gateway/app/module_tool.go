5806a37b2b52404bdc4da2bd5b4925a97b55c538
yaobo <yaobo@cplus.com>
2022-03-10 17:57:52 +0800

    限制数据返回数量

--- cmd/gateway/app/module_tool.go
+++ cmd/gateway/app/module_tool.go
@@ -26,11 +26,11 @@ func (m *cremaMarket) Start(c *config.Config, engine *gin.Engine) error {
 	engine.GET("/swap/count", handler.SwapCount)
 	engine.GET("/config", handler.GetConfig)
 	engine.GET("/tvl/24hour", handler.GetTvl)
-	engine.GET("/v2/histogram", handler.GetHistogram)
 	engine.POST("/v2/tvl", handler.GetTvlV2)
 	engine.POST("/v2/vol/24h", handler.Get24hVolV2)
 	engine.POST("/v2/vol", handler.GetVolV2)
 	engine.POST("/v2/kline", handler.GetKline)
+	engine.GET("/v2/histogram", handler.GetHistogram)
 
 	return nil
 }
