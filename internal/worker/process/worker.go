8b87f1bdeefc76bd67112bb1d6f052be2a46c6f3
yaobo <yaobo@cplus.zone>
2022-05-11 20:52:44 +0800

    注释

--- internal/worker/process/worker.go
+++ internal/worker/process/worker.go
@@ -124,7 +124,7 @@ func Init(viperConf *config.Config) error {
 	// create sync transaction cron job
 	syncTransactionJob := NewJobInfo("SyncTvl")
 	job.JobList["SyncKline"] = syncTransactionJob
-	_, err = job.Cron.AddFunc(defaultBaseSpec, CreateSyncKLine)
+	//_, err = job.Cron.AddFunc(defaultBaseSpec, CreateSyncKLine)
 	userSyncTransactionJob := NewJobInfo("UserSync")
 	job.JobList["UserSyncKline"] = userSyncTransactionJob
 	_, err = job.Cron.AddFunc(defaultBaseSpec, CreateUserSyncKLine)
