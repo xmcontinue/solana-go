e8bb15f7ed8c1c996d493df695be4acce970f51b
maxinyu <408567964@qq.com>
2022-05-12 15:29:41 +0800

    开启同步价格至kline

--- internal/worker/process/worker.go
+++ internal/worker/process/worker.go
@@ -116,15 +116,15 @@ func Init(viperConf *config.Config) error {
 	}
 
 	// TODO 临时同步合约价格冗余至交易额表
-	// _, err = job.Cron.AddFunc(getSpec("sync_swap_cache"), SyncPriceToSwapKLine)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	_, err = job.Cron.AddFunc(getSpec("sync_swap_cache"), SyncPriceToSwapKLine)
+	if err != nil {
+		panic(err)
+	}
 
 	// create sync transaction cron job
 	syncTransactionJob := NewJobInfo("SyncTvl")
 	job.JobList["SyncKline"] = syncTransactionJob
-	//_, err = job.Cron.AddFunc(defaultBaseSpec, CreateSyncKLine)
+	// _, err = job.Cron.AddFunc(defaultBaseSpec, CreateSyncKLine)
 	userSyncTransactionJob := NewJobInfo("UserSync")
 	job.JobList["UserSyncKline"] = userSyncTransactionJob
 	_, err = job.Cron.AddFunc(defaultBaseSpec, CreateUserSyncKLine)
