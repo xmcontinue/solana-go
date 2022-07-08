f044ff8513bf21d8a27a59857531688a09113f34
yaobo <yaobo@cplus.zone>
2022-07-08 20:01:20 +0800

    注释统计24小时的worker

--- internal/worker/process/worker.go
+++ internal/worker/process/worker.go
@@ -94,10 +94,10 @@ func Init(viperConf *config.Config) error {
 	//	panic(err)
 	// }
 
-	_, err = job.Cron.AddFunc(getSpec("sync_swap_cache"), swapAddressLast24HVol)
-	if err != nil {
-		panic(err)
-	}
+	//_, err = job.Cron.AddFunc(getSpec("sync_swap_cache"), swapAddressLast24HVol)
+	//if err != nil {
+	//	panic(err)
+	//}
 
 	_, err = job.Cron.AddFunc(getSpec("sync_swap_cache"), sumTotalSwapAccount)
 	if err != nil {
