a911ea789092206838527e8eeed48f03a78384cf
yaobo <yaobo@cplus.zone>
2022-10-14 20:09:06 +0800

    更新合约地址到测试环境

--- chain/sol/parse/parse_v2.go
+++ chain/sol/parse/parse_v2.go
@@ -8,9 +8,11 @@ import (
 	event "git.cplus.link/crema/backend/chain/event/parser"
 )
 
-const cremaSwapProgramAddressV2 = "CLMM9tUoggJu2wagPkkqs9eFG4BWhVBZWkP1qv3Sp7tR"
+// 正式环境
+//const cremaSwapProgramAddressV2 = "CLMM9tUoggJu2wagPkkqs9eFG4BWhVBZWkP1qv3Sp7tR"
 
-//const cremaSwapProgramAddressV2 = "CcLs6shXAUPEi19SGyCeEHU9QhYAWzV2dRpPPNA4aRb7"
+// dev 环境
+const cremaSwapProgramAddressV2 = "CcLs6shXAUPEi19SGyCeEHU9QhYAWzV2dRpPPNA4aRb7"
 
 type Txv2 struct {
 	SwapRecords      []*SwapRecordV2
