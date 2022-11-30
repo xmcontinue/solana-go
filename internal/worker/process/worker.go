5ea7d4a9ad26e9d5d09c97c43d3d1b69c095c1f7
yaobo <yaobo@cplus.com>
2022-11-30 14:43:05 +0800

    分表

--- internal/worker/process/worker.go
+++ internal/worker/process/worker.go
@@ -76,17 +76,16 @@ func Init(viperConf *config.Config) error {
 	if err != nil {
 		return errors.Wrap(err)
 	}
-	logger.Info("migrate begin")
-	// 先做数据迁移，做完后才能获取新数据
-	err = migrate()
-	if err != nil {
-		return errors.Wrap(err)
-	}
+	//logger.Info("migrate begin")
+	//// 先做数据迁移，做完后才能获取新数据
+	//err = migrate()
+	//if err != nil {
+	//	return errors.Wrap(err)
+	//}
+	//
+	//logger.Info("migrate done")
+	//logger.Info("migrate done")
 
-	logger.Info("migrate done")
-	logger.Info("migrate done")
-
-	return nil
 	err = conf.UnmarshalKey("collection_mint", &collectionMint)
 	if err != nil {
 		return errors.Wrap(err)
