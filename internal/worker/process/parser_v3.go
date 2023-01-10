dbd62513bb3f6628c0bae1f9d0f090bcefd600fe
yaobo <yaobo@cplus.com>
2023-01-10 23:22:52 +0800

    fix

---
+++ internal/worker/process/parser_v3.go
@@ -0,0 +1,71 @@
+package process
+
+import (
+	"context"
+	"time"
+
+	"git.cplus.link/go/akit/errors"
+	"git.cplus.link/go/akit/logger"
+	"git.cplus.link/go/akit/util/decimal"
+
+	"git.cplus.link/crema/backend/chain/sol/parse"
+	model "git.cplus.link/crema/backend/internal/model/market"
+)
+
+// token a volume token b volume ,fee amount， vol for usd
+func syncPrice(swapAccount string, t time.Time) (decimal.Decimal, decimal.Decimal, decimal.Decimal, decimal.Decimal, error) {
+	var volForUsd, swapAVolForUsd, swapBVolForUsd, swapFeeVolForUsd decimal.Decimal
+
+	for {
+		transactions, err := model.QuerySwapTransactionsV2(context.Background(), 100, 0, model.SwapAddressFilter(swapAccount), model.NewFilter("block_time > ?", t), model.OrderFilter("id asc"))
+		if err != nil {
+			if errors.Is(err, errors.RecordNotFound) {
+				break
+			}
+			return decimal.Zero, decimal.Zero, decimal.Zero, decimal.Zero, err
+		}
+
+		if len(transactions) != 0 {
+			t = *transactions[len(transactions)-1].BlockTime
+		}
+
+		for _, transaction := range transactions {
+			tx := parse.NewTxV2()
+
+			err := tx.ParseSwapV2(transaction.Msg)
+			if err != nil {
+				if errors.Is(err, errors.RecordNotFound) {
+					continue
+				}
+				logger.Error("sync transaction id err", logger.Errorv(err))
+				return decimal.Zero, decimal.Zero, decimal.Zero, decimal.Zero, errors.Wrap(err)
+			}
+
+			if len(tx.SwapRecords) == 0 {
+				continue
+			}
+
+			for _, v := range tx.SwapRecords {
+				var tempAVolForUsd, tempBVolForUsd, feeVolForUsd decimal.Decimal
+				if v.Direction == 0 {
+					tempAVolForUsd = v.AmountIn.Mul(transaction.TokenAUSD)
+					tempBVolForUsd = v.AmountOut.Mul(transaction.TokenBUSD)
+					feeVolForUsd = v.FeeAmount.Mul(transaction.TokenAUSD)
+				} else {
+					tempAVolForUsd = v.AmountOut.Mul(transaction.TokenAUSD)
+					tempBVolForUsd = v.AmountIn.Mul(transaction.TokenBUSD)
+					feeVolForUsd = v.FeeAmount.Mul(transaction.TokenBUSD)
+				}
+
+				volForUsd = volForUsd.Add(tempAVolForUsd)
+				swapAVolForUsd = swapAVolForUsd.Add(tempAVolForUsd)
+				swapBVolForUsd = swapBVolForUsd.Add(tempBVolForUsd)
+				swapFeeVolForUsd = swapFeeVolForUsd.Add(feeVolForUsd)
+
+			}
+
+		}
+	}
+
+	return volForUsd, swapAVolForUsd, swapBVolForUsd, swapFeeVolForUsd, nil
+}
--- internal/worker/process/swap_count.go
+++ internal/worker/process/swap_count.go
@@ -3,6 +3,7 @@ package process
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"sort"
 	"strconv"
 	"strings"
@@ -93,6 +94,35 @@ func SwapTotalCount() error {
 		tvlInUsd = tokenATvl.Add(tokenBTvl)
 		tokenAVol, tokenBVol := swapCountTotal.TokenAVolumeForUsd.Round(countDecimal), swapCountTotal.TokenBVolumeForUsd.Round(countDecimal)
 		if v.Version == "v2" {
+			t := time.Now()
+			if swapCount24h.TxNum != 0 {
+				swapCount24h.VolInUsdForContract, swapCount24h.TokenAVolume, swapCount24h.TokenBVolume, swapCount24h.FeeAmount, err = syncPrice(v.SwapAccount, before24hDate)
+				if err != nil {
+					return errors.Wrap(err)
+				}
+			}
+
+			if swapCount7d.TxNum != 0 {
+				swapCount7d.VolInUsdForContract, swapCount7d.TokenAVolume, swapCount7d.TokenBVolume, swapCount7d.FeeAmount, err = syncPrice(v.SwapAccount, before7dDate)
+				if err != nil {
+					return errors.Wrap(err)
+				}
+			}
+
+			if swapCount30d.TxNum != 0 {
+				swapCount30d.VolInUsdForContract, swapCount30d.TokenAVolume, swapCount30d.TokenBVolume, swapCount30d.FeeAmount, err = syncPrice(v.SwapAccount, before30dDate)
+				if err != nil {
+					return errors.Wrap(err)
+				}
+			}
+
+			if swapCountTotal.TxNum != 0 {
+				swapCountTotal.VolInUsdForContract, swapCountTotal.TokenAVolume, swapCountTotal.TokenBVolume, swapCountTotal.FeeAmount, err = syncPrice(v.SwapAccount, time.Now().Add(-24*time.Hour*365))
+				if err != nil {
+					return errors.Wrap(err)
+				}
+			}
+			fmt.Println("时间：", time.Now().Sub(t))
 			volInUsd = swapCountTotal.VolInUsdForContract
 		} else {
 			volInUsd = tokenAVol.Add(tokenBVol)
